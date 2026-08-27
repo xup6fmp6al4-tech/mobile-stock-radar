14cbe5809ae8dda0911e54e2ab9ab228395144ee
xup6fmp6al4-tech <xup6fmp6al4@gmail.com>
2026-08-28 00:50:50 +0800

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE='stock-radar-v0.5';
+const CACHE='stock-radar-v0.6';
 const SHELL=['/','/manifest.webmanifest','/static/icon-192.png','/static/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
